125c020dab1005526abb12dbd8a7384910adfdc5
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-07 18:04:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('H1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML='Abcd is the champion!';

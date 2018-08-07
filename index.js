23d2c1523df3ac9c689c544041a7f0152032eb60
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-07 18:05:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute('id','victory');
+newHeader.innerText='Abcd is the champion!';
